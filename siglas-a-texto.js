cdda96e492bc762c1bf9097b2d8e22daed83917b
manyNeurons <ferranramirezmarti@gmail.com>
2022-05-08 01:37:43 +0200

    mejoras

--- siglas-a-texto.js
+++ siglas-a-texto.js
@@ -14,8 +14,8 @@ class AbrToTextTitle extends React.Component {
       let words = this.props.text.split(' ')
       words.forEach(word =>{
          let letter = this.props.abrev.charAt(i)
-         if(letter == word.charAt(0)) {
-            labels.push(<label key={letter} className={"letra"}>{letter}</label>)
+         if(letter == word.charAt(0)) { // debería ignorar las mayúsculas
+            labels.push(<label key={letter+i} className={"letra"}>{letter}</label>)
             labels.push(<label key={word} className={"texto"}>{word.substring(1)} </label>)
             i++
             return
@@ -31,4 +31,4 @@ class AbrToTextTitle extends React.Component {
    }
 }
 
-ReactDOM.render(<AbrToTextTitle abrev="CIIP" text="Campus Internacional de Inteligencia y Pericia" />, domContainer)
+ReactDOM.render(<AbrToTextTitle abrev="AHMHNBMS" text="Ano Hi Mita Hana no Namae o Bokutachi wa Mada Shiranai" />, domContainer)
